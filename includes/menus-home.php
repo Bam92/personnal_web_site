08688753572b842bcb986487ca9261fde43b1c10
Bam92 <me@abelmbula.url.ph>
2016-08-30 14:14:07 +0200

    arranging php structure

---
+++ includes/menus-home.php
@@ -0,0 +1,28 @@
+<div class="container-fluid">
+        
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <!--Responsive nav bar-->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                
+            </div>
+            <div class="container">
+                <div class="navbar-header"><a href="../index.php" class="navbar-brand">Bam92</a></div>
+                <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav pull-right">                
+                    
+                    <li><a href="templates/training.php">Formation</a> </li>
+                    <li> <a href="templates/about.php">A propos</a></li>
+                    <li> <a href="templates/project.php">Portfolio</a></li>
+                    <li class="disabled"> <a href="#">Blog</a></li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+        
+    </div>
---
+++ index.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
+    <meta name="author" content="Abel Lifaefi" />
+    <title>Abel Lifaefi Mbula | Site personnel</title>
+
+    <!-- CSS  -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Bitter' rel='stylesheet' type='text/css'>
+    <link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="screen,projection" />
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
+    
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+</head>
+
+<body>
+<?php include ("includes/menus-home.php"); ?>
+    
+    <!-- Breadcrumb
+    ==============================================================
+    <div class="container">
+         <div class="row">
+            <div class="breadcrumb">
+                <ol class="breadcrumb">
+                  <li><a href="index.htm">Accueil</a></li>
+                  <li class="active">Accueil</li>
+
+                </ol>
+            </div>
+        </div>
+        
+    </div>-->
+    
+    <!-- Jumbotron
+    ==============================================================-->
+    <header class="jumbotron">
+        <div class="container">
+            <div class="row row-header">
+                <div class="col-md-4"><img src="img/profil-abel-2016.png" alt="profil" class="media-object img-thumbnail" height="300" width="300">
+                    <h3>Abel Lifaefi Mbula</h3>
+                    <h4>Developpeur Full Stack et Formateur
+                    </h4>
+                </div>
+                <div class="col-xs-12 col-md-8">
+                    <h1>Bienvenue chez moi!</h1>
+                   
+                    <p>Je travaille dans le monde merveilleux de l'<abbr title="informatique">IT</abbr> depuis 6 années déjà. </p>
+                    <ul style="font-size: 1.3em;">
+                        <li><strong>enseignement</strong>: informatique et web</li>
+                        <li><strong>services</strong>: maintenance des parcs, conception des sites web...</li>
+                        <li><strong>conseil</strong></li>
+                    </ul>
+                    <p>
+                        Vous avez tout intérêt à confier vos projets informatiques à un <strong>professionnel! </strong>Non? :)
+                    </p>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="container" role="main">
+        
+        
+        <section id="activity" class="section">
+            <h2 class="text-center">Ce que je fais en bref...</h2>
+			<div class="row">
+				<div class="col-md-12 text-center section-intro">
+                    <p>Mon quotidien est rempli de missions diverses et variées: gérer des projets informatiques, partager ma connaissance, animer/modérer des forums, proposer des conseils et faire la veille technologique sur le développement web et mobile </p>
+                </div>
+			</div>
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="text-center" style="color:#ff9800">
+                    <span class ="glyphicon
+                        glyphicon-education text-center" style="size:2em;"></span><h3>Formation, conférence</h3></div>
+                   
+                    <p>En moyenne 10 personnes sont formees et 1 conference ou aterlier anime chaque mois</p><p> Je traite de la bureautique, d'Android, de <abbr title="WordPress">WP</abbr> et de la programmation web.</p>
+                </div>
+                <div class="col-md-3">
+                    <div class="text-center" style="color:#ff9800"><span class ="glyphicon
+                        glyphicon-user"></span><h3>Conseil</h3></div>
+                    <p>Ma longue expérience dans l'ingénierie informatique me permet d'exercer le métier de <strong>consultant IT</strong></p>
+                    <p>Mes conseils portent sur le choix de l'<abbr title="Systeme d'exploitation">OS</abbr>, d'un <abbr title="Systeme de gestion des bases de donnees">SGBD</abbr>, de l'hébergment web... </p>
+                </div>
+                <div class="col-md-3">
+                    <div class="text-center" style="color:#ff9800"><span class ="glyphicon
+                        glyphicon-usd text-center"></span><h3>Freelance</h3></div>
+                    
+                    <p>Je travaille actuellement sur différentes msissions web en freelance et entreprends de monter une <abbr title="Société de service et d'ingénieurie informatique">SS2I</abbr> très prochainement</p>
+                    <p>Mon tarif horaire moyen s'élève à <strong>12,5 €</strong></p>
+                </div>
+                <div class="col-md-3">
+                    <div class="text-center" style="color:#ff9800"><span class ="glyphicon
+                        glyphicon-comment"></span ><br> <h3>Communauté</h3></div>
+                    <p>Je suis actif dans plusieurs grands forums informatiques où je passe en moyenne 2 à 3 heures/semaine: <a href="https://www.openclassrooms.com">OpenClassrooms</a>, <a href="https://www.developpez.com">Developpez.com</a>, <a href="https://www.coursera.org">Coursera</a>, ...</p>
+                    <p>Je suis auteur d'un <abbr title="Cours en ligne massif et ouvert">MOOC</abbr></p>
+                </div>
+                
+            </div>
+            <div class="text-center section-end"> <a href="about.html" class="more">En savoir plus...</a></div><!--Link to about.html-->
+        </section>
+        
+        <section id="project" class="section">
+            <h2 class="text-center">Extrait de mes projets...</h2>
+            
+                <div class="col-xs-12 text-center section-intro">
+                    <p>Retrouvez quelques-unes de mes réalisations, de l'enseignement aux projets web</p>
+                    
+                </div>
+                <div class="row">
+                <div class="col-md-6" style="margin-bottom:40px;"><img src="img/project/confusion.PNG" alt="confusion" height="400" width="650"></div>
+                <div class="col-md-offset-1 col-md-5" style="margin-bottom:40px;">
+                    <div>
+                        <p><strong class="col-md-3">Mission</strong> : <span style="border-bottom:2px dotted orange;">Conception site web</span></p>
+                        <p><strong class="col-md-3">Client</strong> : <span style="border-bottom:2px dotted orange;">ConFusion</span></p>
+                        <p><strong class="col-md-3">Technologies</strong> : <span style="border-bottom:2px dotted orange;">HTML5, CSS3, JS, PHP5, Bootstrap, jQuery</span></p>
+                        <h4>Commentaires</h4>
+                        <p><strong>ConFusion</strong> est un restaurant indien qui souhaitait avoir une présence sur la toile afin de permettre aux clients de voir ce qu'il fait et de pouvoir faire des reservation. La contrainte technique était que le site devrait être adapté à tous les médias (donc responsive)</p>
+                        <p>Après plusieures heures de concertation pour l'élaboration du cahier des charges, je suis parvenu à produire un site fonctionnel et adapté à toutes les résolutions d'écran; à la grande satisfaction de mon client</p>
+                        <p>Visiter le <a href="#">site</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-5">
+                    <div>
+                        <p><strong class="col-md-3">Mission</strong> : <span style="border-bottom:2px dotted orange;">Foramation du personnel</span></p>
+                        <p><strong class="col-md-3">Client</strong> : <span style="border-bottom:2px dotted orange;"><abbr title="Association des femmes d'affaire du Congo">AFAC</abbr></span></p>
+                        <p><strong class="col-md-3">Technologies</strong> : <span style="border-bottom:2px dotted orange;">Microsoft Office</span></p>
+                        <h4>Commentaires</h4>
+                        <p>AFAC est une association des femmes d'affaire du Congo (Kinshasa). Dans le cadre de la promotion des TIC au près de le femme congolaise, j'ai été heureux de pouvoir gagner ce marché afin de former les femmes de la société civile et députées provinciales à Kisangani</p>
+                    </div>
+                </div>
+                 <div class="col-md-offset-1 col-md-6"><img src="img/project/afac.JPG" alt="afac" width="100%" height="80%" style="margin-bottom:40px;"></div>
+            </div>
+            
+            <div class="text-center section-end"> <a href="about.html" class="more">En savoir plus...</a></div><!--Link to about.html-->
+        </section>
+
+        <section id="say" class="section">
+            <h2 class="text-center">Ce qu'ils disent de moi...</h2>
+            <div class="row">
+				<div class="col-md-12 text-center section-intro">
+                    <p>J'aime ce que je fais, j'y mets du sérieux, j'ai des sourires aux lèvres et une petite dose d'humour...Mes clients, mes étudiants et mes collègues aiment ça et c'est donc tout naturellement qu'ils ne cessent de me recommander
+                    
+                </div>
+			</div>
+            <div class="row">
+                <div class="col-md-2"><img src="img/profil.jpg" alt="image1" class="img-circle">
+                    <h4>Pierre Mukadi</h4>
+                    <h5>Admin systeme et reseau, <strong>UNIKIS</strong></h5>
+                </div>
+                <div class="col-md-4">
+                    <p>Lorem ipsom dokjkwje ejkwnkd kjdnskjhkjd kjnksnkn Lorem ipsom dokjkwje ejkwnkd kjdnskjhkjd kjnksnkn
+                    Lorem ipsom dokjkwje ejkwnkd kjdnskjhkjd kjnksnknLorem ipsom dokjkwje ejkwnkd kjdnskjhkjd kjnksnkn Lorem ipsom dokjkwje </p>
+                    <p>Lorem ipsom dokjkwje ejkwnkd kjdnskjhkjd kjnksnkn Lorem ipsom dokjkwje ejkwnkd kjdnskjhkjd kjnksnkn
+                    Lorem ipsom dokjkwje</p>
+                </div>
+                <div class="col-md-2"><img src="img/profil.jpg" alt="image2" class="img-circle">
+                    <h4>Catherine Darbelly</h4>
+                    <h5>Chef d'entreprise, <strong>CD Training</strong></h5>
+                </div>
+                <div class="col-md-4">
+                    <p>m dokjkwje ejkwnkd kjdnskjhkjd kjnks kjkwje ejkwnkd kjdnskjhkjd kjnksnkn</p>
+                    <p>Lorem ipsom dokjkwje ejkwnkd kjdnskjhkjd kjnksnkn Lorem ipsom dokjkwje ejkwnkd kjdnskjhkjd kjnksnkn
+                    Lorem ipsom dokjkwje</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-2"><img src="img/profil.jpg" alt="image3" class="img-circle">
+                    <h4>Patience Kavira</h4>
+                    <p>Chercheuse, <strong>CSB</strong></p>
+                </div>
+                <div class="col-md-4">
+                    <p>Lorem ipsom dokjkwje ejkwnkd kjdnskjhkjd kjnksnkn </p>
+                </div>
+                <div class="col-md-2"><img src="img/profil.jpg" alt="image4" class="img-circle">
+                    <h4>Michel Longombo</h4>
+                    <p>Gestionnaire, <strong>NTEB</strong></p>
+                </div>
+                <div class="col-md-4">
+                    <p>Lorem ipsom dokjkwje ejkwnkd kjdnskjhkjd kjnksnkn </p>
+                </div>
+            </div>
+            <div class="text-center section-end"> <a href="testimony.html" class="more">En savoir plus...</a></div><!--Link to testimony.html-->
+        </section>
+        <section id="blog" class="section">
+            <h2 class="text-center">Sur le blog et les réseaux sociaux...</h2>
+            <div class="row">
+				<div class="col-md-12 text-center section-intro">
+                    <p>J'anime un web blogue qui constitue mon pense-bête informatique. Je tweete, je poste mais je ne fais pas de selfie</p>
+                    
+                </div>
+                
+                <div class="col-md-6">
+                    <h4>Derniers tweets</h4>
+                    <ul>
+                        <li>Lorem</li>
+                        <li>Lorem</li>
+                        <li>Lorem</li>
+                    </ul>
+                </div>
+                <div class="col-md-6">
+                    <h4>Derniers billets</h4>
+                    <ul>
+                        <li>Lorem</li>
+                        <li>Lorem</li>
+                        <li>Lorem</li>
+                    </ul>
+                </div>
+			</div>
+         
+        </section>
+    </div>
+    
+   <?php include ("includes/footer.php"); ?>
+    
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="js/jquery.js"></script> <!--jQuery in local-->
+    
+    <!-- Javascript for Bootstrap -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.js"></script><!--JS for Bootstrap in local-->
+</body>
+
+</html>
